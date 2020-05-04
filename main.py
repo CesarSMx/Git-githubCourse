054b7910f39d57da538c34f8f69d723f01de3eac
Cesar Sosa <cesar.sosa.zg@gmail.com>
2020-05-04 11:10:06 -0500

    Agregue una segunda linea pero en ingles

--- main.py
+++ main.py
@@ -1,3 +1,2 @@
 # this is a test file for the Platzi git code
-#esta es una segunda linea agregada al archivo
-#esta es la tercera linea agregada al archivo
+# this is the second line in file but in english
